fe5742348f199033cb0cc4fc5dff1dc7f0d35752
Saleh Arif <60255252+SalehArif@users.noreply.github.com>
2021-04-13 02:21:45 +0500

    Update Scheduler.py

--- Scheduler.py
+++ Scheduler.py
@@ -105,4 +105,4 @@ class gui:
         tkinter.messagebox.showinfo("Disclaimer","Sign in your Whatsapp Web on your default browser before scheduling a message.")
 
 gui()
-# pywhatkit.sendwhatmsg('+923156872207','meep morp, I\'m a bot again.',21,50)
+# pywhatkit.sendwhatmsg('+923341272207','meep morp, I\'m a bot again.',21,50)
